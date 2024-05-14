15834b1c938cce091284ec6f950b86f1f72e0e3b
Piage <piageaguirre@gmail.com>
2024-05-15 00:32:57 +0800

    Search

--- Search.php
+++ Search.php
@@ -5,22 +5,28 @@
 <script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/1.8.2/jquery.min.js"></script>
 <script>
 function fill(value){
-    $('#search').val(value);
-    $('#display').hide();
+    $('#searchBox').val(value);
+    $('#searchResults').hide();
 }
 $(document).ready(function(){
+    console.log("test 1");
     $("#searchBox").keyup(function(){
-        var name = $("#search").val();
+        console.log("test 2");
+        var name = $('#searchBox').val();
         if (name=="") {
-            $("#searchResults").html("");
+            console.log("test 3");
+            $('#searchResults').html("");
         } else {
+            console.log("test 4");
             $.ajax({
-                type: "POST", url:"getSearchResults.php", 
+                type: "POST", 
+                url:"getSearchResults.php", 
                 data: {
-                    search:name
+                    search: name
                 },
                 success: function(html){
-                    $("#display").html(html).show();
+                    console.log("test 5");
+                    $('#searchResults').html(html).show();
                 }
             });
         }
@@ -38,8 +44,8 @@ $(document).ready(function(){
 <html>
     <body>
         <form name="searchBar">
-            <input type="text" size="30" onkeyup="" id="searchBox">
-            <div id="searchResults"></div>
+            <input type="text" size="30" id="searchBox">
+            <div id="searchResults">Hello</div>
         </form>
     </body>
 </html>
