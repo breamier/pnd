6512eddc768a9967b3eef19f28b236dec249c707
Aimee Rae Bayle <bayleaimeerae3@gmail.com>
2024-05-24 18:53:30 +0800

    Add Contact and Affiliation to Database

--- addContact.php
+++ addContact.php
@@ -1,20 +1,26 @@
 <?php
 include 'Connect.php';
 
-if($_SERVER["REQUEST_METHOD" == "POST"]){
+if($_SERVER["REQUEST_METHOD"] == "POST"){
     $fname = $_POST["fname"];
     $lname = $_POST["lname"];
     $birthdate = $_POST["birthdate"];
     $gender = $_POST["gender"];
-    $infoTypes = $_POST['infoType'];
-    $infoDescs = $_POST['infoDesc'];
+    $infoTypes = $_POST["infoType"]; 
+    $infoDescs = $_POST["infoDesc"];
 
-    
-    for ($i = 0; $i < count($infoTypes); $i++) {
-        $infoType = $infoTypes[$i];
-        $infoDesc = $infoDescs[$i];
+    list($year, $month, $day) = explode('-', $birthdate);
+
+    $sql = "INSERT INTO `Individual`(`Fname`, `LName`, `Month`, `Day`, `Year`, `IndividualID`, `Gender`)
+            VALUES ('$fname', '$lname', '$month', '$day', '$year', '', '$gender')";
+
+    if($conn->query($sql) === TRUE){
+        echo "Added Individual Info";
+    } else {
+        echo "Error";
     }
 
-    $sql = "INSERT INTO ";
+    $idv_id = $conn->insert_id; // used later on for adding contact info and affiliations   
+
 }
 ?>
--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 function add_field(){
     var div = document.createElement('div');
     var html='<label for="infoType">Contact: </label>\
-                <select class="expand" name="infoType">\
+                <select class="expand" name="infoType[]">\
                     <option value="" disabled="">--Select Type--</option>\
                     <option value="phoneNum">Phone Number</option>\
                     <option value="email">Email</option>\
@@ -11,7 +11,7 @@ function add_field(){
                     <option value="website">Website</option>\
                     <option value="others">Others</option>\
                 </select>\
-                <input type="text" id="infoDesc" name="infoDesc">\
+                <input type="text" id="infoDesc" name="infoDesc[]">\
                 <button onclick="remove_field(this)">Remove</button>';
     div.innerHTML = html;
     document.getElementById('contactInfo').appendChild(div);
