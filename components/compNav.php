7cfd28b63333d7406bc01fb4d460558d016420a9
Aimee Rae Bayle <bayleaimeerae3@gmail.com>
2024-05-30 02:41:33 +0100

    Navbar Changes

--- components/compNav.php
+++ components/compNav.php
@@ -4,7 +4,7 @@
         <a class="logo" href="#"><img src="images/logo.png" class="logoimg"><span>Personal Networking Database</span></a>
     </div>
     <nav>
-        <ul>
+        <ul style="list-style-type: none;">
             <a href="search.php"><li>Search</a></li>
             <a href="index.php"><li>Home</a></li>
         </ul>
